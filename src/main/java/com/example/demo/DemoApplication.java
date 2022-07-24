a2595a73510d1ef0cc187ce3507955b45a633fad
federico <fe.caciapuoti@gmail.com>
2022-07-24 22:35:00 +0000

    Removing old code

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -16,48 +16,4 @@ public class DemoApplication {
 		//generator.allCombinazioni(originalArray);
 		generator.kCombinazioni(2,originalArray,new Integer[0]);
 	}
-
-	public static void combinazioni(int k, Integer[] ar, Integer[] ar2, int last) {
-		if (k == 0)
-			print(ar2);
-		else {
-			for (int i = last; i < ar.length; i++ ) {
-				combinazioni(k-1,ar,add(ar2,ar[i]), ++last);
-			}
-		}
-	}
-
-	public static void disposizioni(int k, Integer[] ar, Integer[] ar2) {
-		if (k == 0)
-			print(ar2);
-		else {
-			for (int i = 0; i < ar.length; i++ ) {
-				disposizioni(k-1,reduce(ar,i),add(ar2,ar[i]));
-			}
-		}
-	}
-
-	public static void print(Integer[] a) {
-		System.out.print("(");
-		for (int i = 0; i < a.length; i++) {
-			System.out.print(a[i]);
-			if (i != a.length-1)
-				System.out.print(",");
-		}
-		System.out.print(")");
-		System.out.println("");
-	}
-
-	public static Integer[] reduce(Integer[] arr, int elementToRemove ) {
-		Integer[] copyArray = new Integer[arr.length - 1];
-		System.arraycopy(arr, 0, copyArray, 0, elementToRemove);
-		System.arraycopy(arr, elementToRemove + 1, copyArray, elementToRemove, arr.length - elementToRemove - 1);
-		return copyArray;
-	}
-
-	public static Integer[] add(Integer[] arr, int elementToAdd) {
-		Integer[] copyArray = Arrays.copyOf(arr, arr.length + 1);
-		copyArray[copyArray.length-1] = elementToAdd;
-		return copyArray;
-	}
 }
